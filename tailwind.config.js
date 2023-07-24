b32fae439a324b3d9a611b1375010a1a23b1859c
Chiamaka <amikable97@gmail.com>
2023-07-24 22:42:33 +0200

    added a build folder with style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
+
 module.exports = {
-  content: ["*.{html,js, ts}"],
+  content: ["*.{html,js, ts, jsx, tsx}",
+],
+darkMode: 'class',
   theme: {
+    fontFamily: {
+      'body': 'Poppins, sans-serif'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+    },
     extend: {},
   },
   plugins: [],
